2a2ddd8a62c89b8cd827ba847c1dd8475f93140a
rapimatt <raphtriv@gmail.com>
2025-11-16 12:19:13 +0800

    Update Untitled-1.c

--- Untitled-1.c
+++ Untitled-1.c
@@ -13,6 +13,7 @@ int main(void) {
     printf("Initial: %c\n", initial);
     printf("Space: %lf\n", space);
     printf("Victor marie");
+    printf("Konsehal krok");
     printf("Sirang plakak");
     printf("addclb");
 
